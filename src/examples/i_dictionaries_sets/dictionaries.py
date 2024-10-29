f477f27c5562d5174bfb92a68a463fc273c18222
JB <150109296+JB52-k3@users.noreply.github.com>
2024-10-29 21:54:39 +0000

    create dic. access

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,2 +1,11 @@
 def test_config():
     return True
+
+def use_a_dictionary():
+    phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Katie'}
+    print(phonebook)
+
+    print(phonebook['555-2222'])
+    print(phonebook['555-1111'])
+    print(phonebook['555-3333'])
+    #print(phonebook['555-1212'])
